7c5340a5db6d609e73ab6229ddb2f0588040e644
lawrence-chege <lawrence.chege@meltwater.org>
2020-03-11 11:44:51 +0000

    get concept from api

--- dist/js/get-concept.js
+++ dist/js/get-concept.js
@@ -1,14 +1,12 @@
-
 const getConcept = () => {
 
     fetch('https://adgency-app.herokuapp.com/v1/graphql', {
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json;'
-        },
-        body: JSON.stringify({ 
-            query:
-                `query{
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json;'
+            },
+            body: JSON.stringify({
+                query: `query{
                     companies{
                     company_name
                     projects{
@@ -52,7 +50,57 @@ const getConcept = () => {
         })
         .then(response => response.json())
         .then(registerData => {
-            companies = registerData
-            console.log(companies)
+            data = registerData["data"]
+            companies = data["companies"]
+            companies.forEach(company => {
+                let company_name = company["company_name"]
+                let projects = company["projects"]
+                if (projects.length > 0) {
+                    projects.forEach(project => {
+                        let project_name = project["name"]
+                        let concepts = project["concepts"]
+                        concepts.forEach(concept => {
+                            let name = concept["name"]
+                            let image = concept["image"]
+                            let category = concept["category"]
+                            let overview = concept["overview"]
+                            let tone = concept["tone"]
+                            let style = concept["style"]
+                            let duration = concept["duration"]
+                            let objectives = concept["objectives"]
+                            console.log(name, image, category, overview, tone, style, duration)
+                            objectives.forEach(objective => {
+                                console.log(objective)
+                            });
+                            let unique_selling_points = concept["unique_selling_points"]
+                            unique_selling_points.forEach(usp => {
+                                console.log(usp)
+                            });
+                            let target_audiences = concept["target_audiences"]
+                            target_audiences.forEach(audience => {
+                                console.log(audience)
+                            });
+                            let platforms = concept["platforms"]
+                            platforms.forEach(platform => {
+                                console.log(platform)
+                            });
+                            let insights = concept["insights"]
+                            insights.forEach(insight => {
+                                console.log(insight)
+                            });
+                            let infos = concept["infos"]
+                            infos.forEach(info => {
+                                console.log(info)
+                            })
+                           
+                        })
+                        console.log(project_name)
+                    })
+                    // console.log(projects)
+                }
+
+                console.log(company_name)
+            });
+
         })
 }
