c38534c9e1c66ba0eb1a8e1b2f2f3b0deed5b48c
lawrence-chege <lawrence.chege@meltwater.org>
2020-04-01 10:24:21 +0000

    attach model

--- assets/scripts/Ideation.js
+++ assets/scripts/Ideation.js
@@ -19,7 +19,7 @@ const genIdeas = () => {
     loader.classList.add("hide")
   }, 15000);
 
-  fetch('http://12d27511.ngrok.io/', {
+  fetch('http://61e82876.ngrok.io/', {
       method: 'POST',
       body: JSON.stringify({
         product: document.getElementById('product').value,
